25f1be19ea439aee09f699ed4f59f3e5981b89c4
castelhano04 <andersoncastelhano2018@gmail.com>
2026-05-04 10:30:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const CACHE_NAME = 'estoque-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
